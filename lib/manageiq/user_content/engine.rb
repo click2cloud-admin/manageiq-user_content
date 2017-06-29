2dc18913b2ace6d83b8a3f4bd854029ffed0f355
Marcel Hild <hild@b4mad.net>
2017-06-29 16:42:43 +0200

    sample_content

--- lib/manageiq/user_content/engine.rb
+++ lib/manageiq/user_content/engine.rb
@@ -2,6 +2,10 @@ module ManageIQ
   module UserContent
     class Engine < ::Rails::Engine
       isolate_namespace ManageIQ::UserContent
+
+      def vmdb_plugin?
+        true
+      end
     end
   end
 end
